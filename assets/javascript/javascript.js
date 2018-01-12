9b7953df4e8be0ddbaf133254745c8af4c616a12
James Frankwich <jamesfrankwich@Jamess-MacBook-Pro.local>
2018-01-11 21:20:44 -0500

    Thursday work

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyDVw2c9yDQHXRAUrP0DXWryYvjo4nMRi7Q",
@@ -7,5 +9,21 @@ var config = {
   storageBucket: "cayenne-9e4bc.appspot.com",
   messagingSenderId: "697519096895"
 };
+
 firebase.initializeApp(config);
 
+  // getting sign up info to register new user
+  $("#sign-up-button").on("click", function(){
+    var usersName = $("#user-name").val().trim();
+    var usersEmail = $("#user-email").val().trim();
+    var usersPassword = $("#user-password").val();
+    console.log(usersName, usersEmail, usersPassword);
+  });
+
+//  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+//    var errorCode = error.code;
+//    var errorMessage = error.message;
+
+//  });
+});
