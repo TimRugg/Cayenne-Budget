fe0a53ae85b62c65e37fc61ba58a9f94b3c205b4
James Frankwich <jamesfrankwich@Jamess-MacBook-Pro.local>
2018-01-11 21:01:01 -0500

    Initial javascript sign up

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDVw2c9yDQHXRAUrP0DXWryYvjo4nMRi7Q",
+  authDomain: "cayenne-9e4bc.firebaseapp.com",
+  databaseURL: "https://cayenne-9e4bc.firebaseio.com",
+  projectId: "cayenne-9e4bc",
+  storageBucket: "cayenne-9e4bc.appspot.com",
+  messagingSenderId: "697519096895"
+};
+firebase.initializeApp(config);
+
