cfc6654e1932caaddc8ea7233a018cfa8fabf972
DESKTOP-AAE5SI9\eoter <e.otero81@gmail.com>
2018-01-16 18:30:49 -0500

    Eddie new files and changes

---
+++ assets/javascript/deals.js
@@ -0,0 +1,17 @@
+
+$.getJSON(
+        "http://www.reddit.com/r/deals.json",
+        function getDeals(data)
+        {
+          $.each(
+            data.data.children.slice(0, 6),
+            function (i, post) {
+              $("#veiwDeals").append( '<br><span class="deal-title">' + post.data.title + '</span>');
+              $("#veiwDeals").append( '<br>' + post.data.domain );
+              $("#veiwDeals").append( '<br><a href="' + post.data.url +'" class="deal-link">' + 'Click Here' + '</a>' );
+              $("#veiwDeals").append( '<hr class="deal-hr">' );
+
+            }
+          )
+        }
+      )
